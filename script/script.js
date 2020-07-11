d8c7fb11aa97902fd0feabc145a12d1e4159d649
ascent2009 <ascent2009@hotmail.com>
2020-07-11 20:22:31 +0300

    feat: add localStorage, form validation, card rendering on main page, image checking in modal

--- script/script.js
+++ script/script.js
@@ -1,6 +1,6 @@
 "use strict";
 
-const dataBase = [];
+const dataBase = JSON.parse(localStorage.getItem("awito")) || [];
 
 const modalAdd = document.querySelector(".modal__add"),
   addAd = document.querySelector(".add__ad"),
@@ -8,46 +8,86 @@ const modalAdd = document.querySelector(".modal__add"),
   modalSubmit = document.querySelector(".modal__submit"),
   catalog = document.querySelector(".catalog"),
   modalItem = document.querySelector(".modal__item"),
-  modalBtnWarning = document.querySelector("modal__btn-warning");
+  modalBtnWarning = document.querySelector(".modal__btn-warning"),
+  modalFileInput = document.querySelector(".modal__file-input"),
+  modalFileBtn = document.querySelector(".modal__file-btn"),
+  modalImageAdd = document.querySelector(".modal__image-add");
+
+const textFileBtn = modalFileBtn.textContent;
+const srcModalImage = modalImageAdd.src;
 
 const elementsModalSubmit = [...modalSubmit.elements].filter(
   (elem) => elem.tagName !== "BUTTON"
 );
 
-const closeModal = function (event) {
+const infoPhoto = {};
+
+const saveDB = () => localStorage.setItem("awito", JSON.stringify(dataBase));
+
+const checkForm = () => {
+  const validForm = elementsModalSubmit.every((elem) => elem.value);
+  modalBtnSubmit.disabled = !validForm;
+  modalBtnWarning.style.display = validForm ? "none" : "";
+};
+
+const closeModal = (event) => {
   const target = event.target;
   if (
-    target.classList.contains("modal__close") ||
-    target === this
-    // target === modalAdd ||
-    // target === modalItem
+    target.closest(".modal__close") ||
+    target.classList.contains("modal") ||
+    event.code === "Escape"
   ) {
-    this.classList.add("hide");
-    // modalAdd.classList.add("hide");
-    // } else if (target.closest(".modal__close") ) {
-    // modalItem.classList.add("hide");
-    // }
-    modalSubmit.reset();
-  }
-};
-
-const closeModalEsc = (event) => {
-  if (event.code === "Escape") {
     modalAdd.classList.add("hide");
     modalItem.classList.add("hide");
-    document.removeEventListener("keydown", closeModalEsc);
+    document.removeEventListener("keydown", closeModal);
+    modalSubmit.reset();
+    modalFileBtn.textContent = textFileBtn;
+    modalImageAdd.src = srcModalImage;
+    checkForm();
   }
 };
 
-modalSubmit.addEventListener("input", () => {
-  const validForm = elementsModalSubmit.every((elem) => elem.value);
+const renderCard = () => {
+  catalog.textContent = "";
+  dataBase.forEach((item, i) => {
+    catalog.insertAdjacentHTML(
+      "beforeend",
+      `
+      <li class="card" data-id="${i}">
+        <img class="card__image" src="data:image/jpeg;base64, ${item.image}" alt="test" />
+        <div class="card__description">
+          <h3 class="card__header">${item.nameItem}</h3>
+          <div class="card__price">${item.costItem} ₽</div>
+        </div>
+      </li>
+  `
+    );
+  });
+};
 
-  modalBtnSubmit.disabled = !validForm;
-  modalBtnWarning.style.display = validForm ? "none" : " ";
-  // if (validForm) {
-  //   modalBtnWarning.classList.remove("hide");
-  // }
+modalFileInput.addEventListener("change", (event) => {
+  const target = event.target;
+  const reader = new FileReader();
+  const file = target.files[0];
+  infoPhoto.filename = file.name;
+  infoPhoto.size = file.size;
+
+  reader.readAsBinaryString(file);
+  reader.addEventListener("load", (event) => {
+    if (infoPhoto.size < 100000) {
+      modalFileBtn.textContent = infoPhoto.filename;
+      infoPhoto.base64 = btoa(event.target.result);
+      modalImageAdd.src = `data:image/jpeg;base64, ${infoPhoto.base64}`;
+    } else {
+      alert("Размер файла превышает допустимый");
+      modalFileBtn.textContent = "Размер файла превышает допустимые 100Кб";
+      modalFileInput.value = "";
+      checkForm();
+    }
+  });
 });
+
+modalSubmit.addEventListener("input", checkForm);
 
 modalSubmit.addEventListener("submit", (event) => {
   event.preventDefault();
@@ -55,9 +95,12 @@ modalSubmit.addEventListener("submit", (event) => {
   for (const elem of elementsModalSubmit) {
     itemObj[elem.name] = elem.value;
   }
+  itemObj.image = infoPhoto.base64;
   dataBase.push(itemObj);
-  modalSubmit.reset();
-  console.log(dataBase);
+  // modalSubmit.reset();
+  saveDB();
+  renderCard();
+  closeModal({ target: modalAdd });
 });
 
 // modalSubmit.addEventListener("submit", () => {
@@ -68,7 +111,7 @@ modalSubmit.addEventListener("submit", (event) => {
 addAd.addEventListener("click", () => {
   modalAdd.classList.remove("hide");
   modalBtnSubmit.disabled = true;
-  document.addEventListener("keydown", closeModalEsc);
+  document.addEventListener("keydown", closeModal);
 });
 
 modalAdd.addEventListener("click", closeModal);
@@ -78,6 +121,8 @@ catalog.addEventListener("click", (event) => {
   const target = event.target;
   if (target.closest(".card")) {
     modalItem.classList.remove("hide");
-    document.addEventListener("keydown", closeModalEsc);
+    document.addEventListener("keydown", closeModal);
   }
 });
+
+renderCard();
